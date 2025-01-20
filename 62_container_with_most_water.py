91150eccf8cd554f38c6a3da594878a1ece8a9f7
Radhika Tekade <tekade.radhika@gmail.com>
2025-01-19 18:21:53 -0800

    Create 62_container_with_most_water

---
+++ 62_container_with_most_water.py
@@ -0,0 +1,23 @@
+# Time complexity - O(n)
+# Space complexity - O(1)
+
+# Approach - Maintain two pointers, increase left pointer when h[l] < h[r]
+# or increase right pointer.
+
+from typing import List
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l = 0
+        r = len(height) - 1
+        maxA = 0
+
+        while l < r:
+            w = r - l
+            h = min(height[l], height[r])
+            maxA = max(maxA, h*w)
+            if height[l] <= height[r]:
+                l += 1
+            else:
+                r -= 1
+
+        return maxA
